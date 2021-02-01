eaeae0daca18f6f912bcfe0d3c075bb0cda95f4d
Sarah <78261341+SarahDieCoolige@users.noreply.github.com>
2021-02-01 16:10:56 +0100

    Create main.cc

---
+++ css/main.cc
@@ -0,0 +1,86 @@
+.chat_ibg{
+  padding: 5px 0px 0px 20px !important;
+  background: transparent;
+}
+
+.superban{
+  color: red;
+}
+
+a.superwhisper{
+  color: green;
+}
+
+.ulinner {
+  overflow: auto !important;
+  overflow-x: hidden !important;
+  padding: 5px 25px 0px 10px;       
+}
+
+.ulinner.headless {
+  top: 90px;
+}
+
+.ulinner.headless.gast {
+  top: 50px ;
+}
+
+/* replace Backgound images with simple color */
+.ww_chat_header{
+  background: #385F7C;
+}
+
+/* replace Backgound images with simple color */
+.ww_chat_footer{
+  background: #385F7C ;
+}
+
+.chat_i #custom_input_text {
+  width: 95%;
+  resize: none;
+  font-size: 1.2em;
+  padding: 2px;
+  margin: 2px;
+}
+
+::placeholder {
+  opacity: 0.4 !important;
+}
+
+.superwhisper::placeholder {
+  opacity: 0.5 !important;
+}
+
+.chat_i {
+  padding-left: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 10px;
+}
+
+.chat_i2 {
+  width: 1%;
+  padding-top: 0;
+  padding-right: 10px;
+}
+
+.chat_i3 {
+  width: 20%;
+}
+
+.chat_i4 {
+    width: 1%;
+}
+.table{
+  border-radius: 10px;
+}
+
+html {
+    overflow: hidden;
+    overflow-x: hidden;
+}
+
+::-webkit-scrollbar {
+    width: 0px;  /* Remove scrollbar space */
+    background: transparent;  
+}    
