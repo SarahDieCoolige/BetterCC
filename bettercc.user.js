331706f3859fcd66ef82e5ddb74fd7131599c82f
Sarah <sarahdiecoolige@users.noreply.github.com>
2022-02-09 17:06:37 +0100

    chore: bump to 1.06

--- bettercc.user.js
+++ bettercc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     BetterCC
-// @version  1.05
+// @version  1.06
 //
 // @include  https://www.chatcity.de/de/cpop.html?&RURL=//www.chatcity.de/
 // @include  https://www.chatcity.de/de/cpop.html?&RURL=//www.chatcity.de/*
@@ -12,8 +12,8 @@
 // @require  https://raw.githubusercontent.com/bgrins/TinyColor/master/tinycolor.js
 // @require  https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.1/dist/GM_wrench.min.js
 //
-// @resource  main_css              https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.05
-// @resource  iframe_css            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.05
+// @resource  main_css              https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.06
+// @resource  iframe_css            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.06
 //
 // @grant    GM_addStyle
 // @grant    GM.setValue
