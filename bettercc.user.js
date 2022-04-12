7c3a38ef53e963f90d5103c0852c8d1ef6a538c0
Sarah <sarahdiecoolige@users.noreply.github.com>
2022-04-12 18:33:24 +0200

    chore: bump to 1.14

--- bettercc.user.js
+++ bettercc.user.js
@@ -2,7 +2,7 @@
 // @name     BetterCC
 // @description  BetterCC is better
 // @author       Sarah
-// @version  1.13
+// @version  1.14
 // @icon https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/BetterCC.png
 //
 // @match  https://www.chatcity.de/de/cpop.html?&RURL=*
@@ -14,8 +14,8 @@
 // @require  https://raw.githubusercontent.com/bgrins/TinyColor/master/tinycolor.js
 // @require  https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.3/dist/GM_wrench.min.js
 //
-// @resource  main_css              https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.13
-// @resource  iframe_css            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.13
+// @resource  main_css              https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.14
+// @resource  iframe_css            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.14
 //
 // @grant    GM_addStyle
 // @grant    GM.setValue
@@ -714,7 +714,7 @@ if (/cpop.html/.test(window.location.href)) {
           "\n\n" +
           "Superwhisper: /sw Sariam" +
           "  |  " +
-          "Superban: /sb Wendigo" +
+          "Ban: /sb Wendigo" +
           "  |  " +
           "Hilfe: /help";
 
@@ -726,12 +726,10 @@ if (/cpop.html/.test(window.location.href)) {
           "Du flüsterst mit " +
           whispernick +
           "..." +
-          "\n\n" +
+          "\n" +
           "Superwhisper aus: /open" +
           "  |  " +
-          "Im Open schreiben: /o Hi All :)" +
-          "  |  " +
-          "Superban: /sb Wendigo" +
+          "/o Hi All :)" +
           "  |  " +
           "Hilfe: /help";
 
