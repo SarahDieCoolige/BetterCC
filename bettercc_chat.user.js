3ec4a264e233be951365e9437526a4da41d8b32a
Sarah <sarahdiecoolige@users.noreply.github.com>
2024-07-26 11:26:48 +0200

    feat: add seperate script for chat iframe

---
+++ bettercc_chat.user.js
@@ -0,0 +1,218 @@
+// ==UserScript==
+// @name     BetterCC_Chat
+// @description  BetterCC is better
+// @author       Sarah
+// @version  1.24
+// @icon https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/BetterCC.png
+//
+
+// @match https://chat.chatcity.de/cc_chat/chatout?*
+// @sandbox JavaScript
+
+
+
+
+//
+// @require  https://code.jquery.com/jquery-3.5.1.min.js
+// @require  https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// @require  https://raw.githubusercontent.com/bgrins/TinyColor/master/tinycolor.js
+// @require  https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.3/dist/GM_wrench.min.js
+//
+// @resource  main_css              https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.24
+// @resource  iframe_css            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.24
+// @resource  iframe_script            https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/iframe_script.js?r=1.24
+
+//
+// @grant    GM_addStyle
+// @grant    GM.setValue
+// @grant    GM.getValue
+// @grant    GM_addValueChangeListener
+// @grant    GM_getResourceText
+// @grant    GM_xmlhttpRequest
+// @grant    GM_log
+// @grant    GM_notification
+// @grant    GM_addElement
+//
+// @downloadURL https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/bettercc_chat.user.js
+// @updateURL https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/bettercc_chat.user.js
+//
+// @supportURL https://github.com/SarahDieCoolige/BetterCC/issues
+// @homepageURL https://github.com/SarahDieCoolige/BetterCC
+
+// ==/UserScript==
+
+/* globals jQuery, $, GM_wrench, ajax, tinycolor*/
+
+(function () {
+
+    "use strict";
+
+    function cclog(str, tag = "BetterCC_CHAT") {
+        GM_log(tag + " - " + str);
+    }
+
+    function ccnotify(message, title = "") {
+        if (enableNotifications) {
+            GM_notification({
+                title: "BetterCC " + title, text: message,
+                onclick: () => {
+                    cclog("Notification clicked.");
+                    window.focus();
+                }
+            });
+        }
+    }
+
+    cclog("Version: " + GM_info.script.version + " - " + window.location.href);
+
+    function printInChat(position = "beforeend", content) {
+        //$("#chatframe").contents().find("body").children().last().append(content);
+
+        document.body.lastChild.insertAdjacentHTML(
+            position,
+            content
+        );
+
+        //let current = document.body.lastChild.innerHTML;
+        //document.body.lastChild.innerHTML+=content;
+    }
+
+    function cclogChat(message, name = "BetterCC", newLineAfterName = true) {
+        message =
+            '<pre id="bccmessage" class="bccmessage" style="white-space: pre-wrap; font-size: 1.2em; width: 70%; display: inline">' +
+            message +
+            "</pre><br>";
+
+        if (name.trim() !== "") {
+            name += ": ";
+            name = '<font color="red"><b>' + name + "</b></font>";
+            if (newLineAfterName)
+                name += "<br>"
+            message = name + message;
+        }
+        printInChat("beforeend", message);
+    }
+
+    let helpStrings = [
+        ["/sw Sariam", "Superwhisper mit Sariam"],
+        ["/o Hi All :)", "Im Open schreiben"],
+        ["/open", "Superwhisper aus"],
+        ["/sb Wendigo", "Einen Arsch für immer ignorieren (noch mal zum entbannen)"],
+        ["/superban", "Arschliste anzeigen"],
+        ["/reload", "Chat neu laden (mimimi)"],
+        ["/settings", "Einstellungen öffnen (irgendwann mal vielleicht^^)"],
+        ["/help", "So zeigst du diese Hilfe hier an"],
+    ];
+
+    let $help = $("<table/>");
+    $help.addClass("helpTable");
+    for (let i = 0; i < helpStrings.length; i++) {
+        $help.append(
+            "<tr><td>" +
+            helpStrings[i][0] +
+            "</td><td>" +
+            helpStrings[i][1] +
+            "</td></tr>"
+        );
+    }
+
+    let helptxt = [
+        "/sw Sariam" + "\t" + "Superwhisper mit Sariam",
+        "/o Hi All :)" + "\t" + "Im Open schreiben",
+        "/open" + " \t\t" + "Superwhisper aus",
+        "/sb Wendigo" +
+        "\t" +
+        "Einen Arsch für immer ignorieren (noch mal zum entbannen)",
+        "/superban" + "\t" + "Arschliste anzeigen",
+        "/reload" + "\t\t" + "Chat neu laden (mimimi)",
+        "/settings" + "\t" + "Einstellungen öffnen (irgendwann mal vielleicht^^)",
+        "/help" + "\t\t" + "So zeigst du diese Hilfe hier an",
+    ].join("\n");
+
+    let helptxtNotify = [
+        "/sw sariam" + " - " + "sw an",
+        "/o hi all :)" + " - " + "ins open",
+        "/open" + " - " + "sw aus",
+        "/sb wendigo" +
+        " - " +
+        "superignore an/aus",
+        "/superban" + " - " + "banliste",
+        "/reload" + " - " + "chat neu laden",
+        "/settings" + " - " + "einstellungen",
+        "/help" + " - " + "hilfe",
+    ].join("\n");
+
+    function printHelp() {
+        //cclogChat(helptxt, "Hilfe");
+        //  printInChat("beforeend", helptxt);
+        ccnotify(helptxtNotify, "Hilfe");
+
+    }
+
+    // window functions
+    var bettercc = (unsafeWindow.bettercc = {});
+
+    if (/cc_chat\/chatout/.test(window.location.pathname)) {
+
+
+        cclog("IFRAME " + window.location.pathname);
+
+        var iframe_css = GM_getResourceText("iframe_css");
+        GM_wrench.addCss(iframe_css);
+        // GM_addStyle(iframe_css);
+
+        window.addEventListener('message', (event) => {
+            GM_log("Bettercc IFRAME" + " - " + "Message Received from " + event.origin + " " + event.data);
+            if (event.origin === 'https://www.chatcity.de') {
+                const message = event.data;
+                switch (message.type) {
+                    case 'changeBackgroundColor':
+                        //document.body.style.backgroundColor = message.color;
+                        GM_log("Bettercc IFRAME" + " - " + "type: " + message.type + "color: " + message.color);
+                        setColor(message.color);
+                        break;
+                    case 'setSuperwhisper':
+                        // ... (logic to update the superwhisper status in the iframe, you'll need to implement this based on how the iframe's chat works)
+                        break;
+                    case 'printInChat':
+                        // ... (logic to display messages within the chat log, you'll need to implement this based on how the iframe's chat works)
+                        break;
+                    // ... (add more cases for other messages)
+                }
+            }
+        }, false,
+        );
+
+
+        function setColor(color) {
+            let $root = $(":root");
+            $root.css("--chatBackground", color);
+        }
+
+        // let listenerId1 = GM_addValueChangeListener("abc", function (key, oldValue, newValue, remote) {
+        //     // Print a message to the console when the value of the "savedTab" key changes
+        //     cclog("IFRAME -  The value of the '" + key + "' key has changed from '" + oldValue + "' to '" + newValue + "'");
+        // });
+
+        // // Add a listener for changes to the "savedTab" key
+        // var listenerId2 = GM_addValueChangeListener("color__janedoe_", function (key, oldValue, newValue, remote) {
+        //     // Print a message to the console when the value of the "savedTab" key changes
+        //     cclog("IFRAME -  The value of the '" + key + "' key has changed from '" + oldValue + "' to '" + newValue + "'");
+        //     document.body.style.backgroundColor = "#" + newValue;
+        // });
+
+        // cclog(listenerId1 + listenerId2);
+
+        // (async function () {
+        //     await GM.setValue("abc", "123");
+        //     let color = await GM.getValue("color__janedoe_", "defcolor");
+        //     let abc = await GM.getValue("abc", "deftest");
+        //     cclog(color + " " + abc);
+        // })();
+
+
+
+
+    }
+
+})();
