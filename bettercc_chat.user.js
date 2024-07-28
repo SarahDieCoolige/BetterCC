8cd9808392d6c886d8ac2f53f28d044ea84e4b2d
Sarah <sarahdiecoolige@users.noreply.github.com>
2024-07-28 15:53:41 +0200

    bump v1.33

--- bettercc_chat.user.js
+++ bettercc_chat.user.js
@@ -2,7 +2,7 @@
 // @name  BetterCC Chat
 // @description  BetterCC is better
 // @author  Sarah
-// @version  1.32
+// @version  1.33
 // @icon  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/BetterCC.png
 //
 // @match  https://chat.chatcity.de/cc_chat/chatout?*
@@ -12,7 +12,7 @@
 // @require  https://raw.githubusercontent.com/bgrins/TinyColor/master/tinycolor.js
 // @require  https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.5/dist/GM_wrench.min.js
 //
-// @resource  iframe_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.32
+// @resource  iframe_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/iframe.css?r=1.33
 //
 // @grant  GM_addStyle
 // @grant  GM.setValue
