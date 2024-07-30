e87b26042e8c9772809cf2ba8e888c199987e180
Sarah <sarahdiecoolige@users.noreply.github.com>
2024-07-30 20:07:02 +0200

    fix domains again

--- bettercc.user.js
+++ bettercc.user.js
@@ -165,7 +165,8 @@
 
   function postMessageToIframe(message) {
     const chatWindow = getChatframe();
-    const targetOrigin = "https://chat.chatcity.de";
+    //const targetOrigin = "https://chat.chatcity.de";
+    const targetOrigin = "https://www.chatcity.de";
     chatWindow.postMessage(message, targetOrigin);
   }
 
