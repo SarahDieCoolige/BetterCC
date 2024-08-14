f0b96afd089ed9f68e0aae1426995a9b58891369
Sarah <sarahdiecoolige@users.noreply.github.com>
2024-08-14 14:44:14 +0200

    bump v1.37

--- bettercc.user.js
+++ bettercc.user.js
@@ -2,7 +2,7 @@
 // @name  BetterCC
 // @description  BetterCC is better
 // @author  Sarah
-// @version  1.36
+// @version  1.37
 // @icon  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/BetterCC.png
 //
 // @match  https://www.chatcity.de/de/cpop.html?&RURL=*
@@ -14,7 +14,7 @@
 // @require  https://raw.githubusercontent.com/bgrins/TinyColor/master/tinycolor.js
 // @require  https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.5/dist/GM_wrench.min.js
 //
-// @resource  main_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.36
+// @resource  main_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/main/css/main.css?r=1.37
 //
 // @grant  GM_addStyle
 // @grant  GM.setValue
