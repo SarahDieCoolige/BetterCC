49844adb6ae1e866636306e572dc26765e9f0efc
sarahdiecoolige <sarahdiecoolige@users.noreply.github.com>
2026-08-30 09:04:31 +0200

    chore: bump to 3.16.0

--- bettercc.user.js
+++ bettercc.user.js
@@ -2,7 +2,7 @@
 // @name  BetterCC (dev)
 // @description  BetterCC
 // @author  Sarah
-// @version      3.15.1
+// @version      3.16.0
 // @icon  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/BetterCC.png
 //
 // @match  https://www.chatcity.de/de/cpop.html
