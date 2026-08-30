52bddabcc87c9a0f2a66ee9c0b5e51177cec6c4d
sarahdiecoolige <sarahdiecoolige@users.noreply.github.com>
2026-08-30 08:58:16 +0200

    style: themed slim scrollbars in shell and chat iframe

--- bettercc.user.js
+++ bettercc.user.js
@@ -14,8 +14,8 @@
 //
 // @require  https://cdn.jsdelivr.net/npm/tinycolor2@1.6.0/dist/tinycolor-min.js
 //
-// @resource  iframe_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/iframe.css?r=04ae35a7
-// @resource  v3_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/v3.css?r=a0b0a061
+// @resource  iframe_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/iframe.css?r=33ccb7af
+// @resource  v3_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/v3.css?r=0f3b00f6
 //
 // @grant  GM_addStyle
 // @grant  GM.setValue
