8c97653c73f55864c9862edb9ae575386862b6ab
sarahdiecoolige <sarahdiecoolige@users.noreply.github.com>
2026-08-30 10:36:42 +0200

    fix: zoom slider uses scheme thumb, no UA border or focus ring

--- bettercc.user.js
+++ bettercc.user.js
@@ -15,7 +15,7 @@
 // @require  https://cdn.jsdelivr.net/npm/tinycolor2@1.6.0/dist/tinycolor-min.js
 //
 // @resource  iframe_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/iframe.css?r=33ccb7af
-// @resource  v3_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/v3.css?r=ce9b2591
+// @resource  v3_css  https://raw.githubusercontent.com/SarahDieCoolige/BetterCC/v3/css/v3.css?r=19ffd40f
 //
 // @grant  GM_addStyle
 // @grant  GM.setValue
